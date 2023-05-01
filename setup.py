fb84757afa359ec063f316e47a8fb45ce278ee1a
morelandcharlotte <70539266+morelandcharlotte@users.noreply.github.com>
2023-04-30 17:02:07 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cs333-finalproject',
+    version='1.0.0',
+    py_modules=['banking'],
+    install_requires=[
+        'random',
+    ],
+    author='Charlotte Moreland',
+    author_email='your@email.com',
+    description='A command-line banking program',
+    keywords='banking',
+    url='https://github.com/morelandcharlotte/CS333-FinalProject',
+)
